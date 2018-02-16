485932fc51b6d099a7e5c1524a0e5eecec0ebaec
Grayson Cowing <grayson.cowing@gmail.com>
2018-02-16 20:02:46 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -6,7 +6,7 @@ end
 
 def summon_captain_planet(array)# code an argument here
   # Your code here
-  array.collect {|thing| thing = "#{thing.capitalize}!"}
+  return array.collect {|thing| thing = "#{thing.capitalize}!"}
 end
 
 def long_planeteer_calls# code an argument here
