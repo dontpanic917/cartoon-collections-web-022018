a0777409b67370fe0cc172e91fc6e96689702157
Grayson Cowing <grayson.cowing@gmail.com>
2018-02-16 21:58:12 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -14,7 +14,10 @@ def long_planeteer_calls(calls)# code an argument here
   calls.length > 4
 end
 
-def find_the_cheese# code an argument here
+def find_the_cheese(array)# code an argument here
   # the array below is here to help
+  permacheese=""
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese_types.each {|cheese| array.include?(cheese) ? return cheese : nil}
+
 end
